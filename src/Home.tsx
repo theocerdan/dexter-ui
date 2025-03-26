8011578137ca81c51002d5cc26e31aa600096e89
theocerdan <tcerdan.pro@gmail.com>
2025-03-26 15:24:14 +0100

    devtool

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,6 @@ import LiquidityPools from "./components/LiquidityPools.tsx";
 import ConnectButtons from "./components/ConnectButtons.tsx";
 import {useAccount} from "wagmi";
 import CreateLiquidityPool from "./components/CreateLiquidityPool.tsx";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import SwapBox from "./components/Swap.tsx";
 
 const Home = () => {
@@ -25,7 +24,6 @@ const Home = () => {
             </> : <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
                 <h1 style={{ fontSize: '30px'}}>You must be connected to interact with Dexter</h1>
             </ div>}
-            <ReactQueryDevtools initialIsOpen={false} />
         </div>
     );
 }
