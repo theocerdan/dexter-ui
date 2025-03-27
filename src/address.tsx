7267aaa92da083e13afd498c13685c0539f18fb2
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 16:27:44 +0100

    vite fix env vars

--- src/address.tsx
+++ src/address.tsx
@@ -1,7 +1,7 @@
 import {Address} from "viem";
 
-export const ROUTER_ADDRESS: Address = import.meta.env.ROUTER_ADDRESS;
-export const USDT_ADDRESS: Address = import.meta.env.USDT_ADDRESS;
-export const WETH_ADDRESS: Address = import.meta.env.WETH_ADDRESS;
+export const ROUTER_ADDRESS: Address = import.meta.env.VITE_ROUTER_ADDRESS;
+export const USDT_ADDRESS: Address = import.meta.env.VITE_USDT_ADDRESS;
+export const WETH_ADDRESS: Address = import.meta.env.VITE_WETH_ADDRESS;
 export const ZERO_ADDRESS: Address = '0x0000000000000000000000000000000000000000';
 export const LOG_DEPTH = 10000n;
