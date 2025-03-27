20e0d560656b8076813e56cc58e1d00ef7918660
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 18:12:10 +0100

    test interval fetching

--- src/repository/LiquidityPoolRepository.ts
+++ src/repository/LiquidityPoolRepository.ts
@@ -11,12 +11,42 @@ export const getAllLiquidityPool = async (): Promise<LiquidityPool[]> => {
 
     const latestBlock = await publicClient.getBlockNumber();
 
+    let depth = 0n;
+    let from = latestBlock;
+    let to = latestBlock - LOG_DEPTH;
+    let lp = [];
+
+    while (true) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        lp = await fetchLiquidityPool(from, to);
+        if (lp.length > 0) {
+            break;
+        }
+        depth += LOG_DEPTH;
+        from = latestBlock - LOG_DEPTH;
+        to = latestBlock - LOG_DEPTH - depth;
+    }
+
+    return Promise.all(lp.map(async (e) => {
+        return {
+            pair: e.pair,
+            tokenA: await getToken(e.tokenA),
+            tokenB: await getToken(e.tokenB),
+        }
+    }));
+
+}
+
+
+const fetchLiquidityPool = async (from: bigint, to: bigint) => {
+
     const logs = await publicClient.getContractEvents({
         address: ROUTER_ADDRESS,
         abi: routerABI,
         eventName: "NewPair",
-        fromBlock: latestBlock - LOG_DEPTH,
-        toBlock: "latest",
+        fromBlock: to,
+        toBlock: from,
     });
 
     const lp: { pair: Address, tokenA: Address, tokenB: Address }[] = logs.map((e) => {
@@ -27,13 +57,5 @@ export const getAllLiquidityPool = async (): Promise<LiquidityPool[]> => {
         }
     }).filter(Boolean);
 
-    return Promise.all(lp.map(async (e) => {
-        return {
-            pair: e.pair,
-            tokenA: await getToken(e.tokenA),
-            tokenB: await getToken(e.tokenB),
-        }
-    }));
-
-
+    return lp;
 }
