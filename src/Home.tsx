b5173bf6af4c7e497044788c4356559edc529b29
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 18:47:32 +0100

    must be connected

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,7 @@ import ConnectButtons from "./components/ConnectButtons.tsx";
 import {useAccount} from "wagmi";
 import CreateLiquidityPool from "./components/CreateLiquidityPool.tsx";
 import SwapBox from "./components/Swap.tsx";
+import {Monitor} from "react95";
 
 const Home = () => {
 
@@ -21,11 +22,23 @@ const Home = () => {
                 <div style={{padding: 30}}>
                     <LiquidityPools />
                 </div>
-            </> : <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-                <h1 style={{ fontSize: '30px'}}>You must be connected to interact with Dexter</h1>
-            </ div>}
+            </> : <Connect />}
         </div>
     );
+}
+
+const Connect = () => {
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+        gap: 50
+    }}>
+        <Monitor></Monitor>
+        <h1 style={{fontSize: '30px', color: 'white'}}>You must be connected to interact with Dexter</h1>
+    </ div>
 }
 
 export default Home;
