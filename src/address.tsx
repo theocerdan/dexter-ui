f487370f70f782d04697314d16e4386e58b7e177
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 16:46:07 +0100

    autre debug

--- src/address.tsx
+++ src/address.tsx
@@ -6,3 +6,4 @@ export const USDT_ADDRESS: Address = import.meta.env.VITE_USDT_ADDRESS;
 export const WETH_ADDRESS: Address = import.meta.env.VITE_WETH_ADDRESS;
 export const ZERO_ADDRESS: Address = '0x0000000000000000000000000000000000000000';
 export const LOG_DEPTH = 10000n;
+console.log("Router r address:", ROUTER_ADDRESS);
