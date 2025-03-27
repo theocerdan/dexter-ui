a810ce77ab8fc2fed5414161bcacdc10fbfe2a32
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 16:50:15 +0100

    retest

--- src/address.tsx
+++ src/address.tsx
@@ -1,9 +1,7 @@
 import {Address} from "viem";
 
-console.log("Router address:", import.meta.env.VITE_ROUTER_ADDRESS);
-export const ROUTER_ADDRESS: Address = import.meta.env.VITE_ROUTER_ADDRESS;
+export const ROUTER_ADDRESS: Address = "0x270ef3A368032Bd2A9b6D79776e854083782c173";
 export const USDT_ADDRESS: Address = import.meta.env.VITE_USDT_ADDRESS;
 export const WETH_ADDRESS: Address = import.meta.env.VITE_WETH_ADDRESS;
 export const ZERO_ADDRESS: Address = '0x0000000000000000000000000000000000000000';
 export const LOG_DEPTH = 10000n;
-console.log("Router r address:", ROUTER_ADDRESS);
