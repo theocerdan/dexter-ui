49505927295d74968cc2aed0dc2da9882bf03f13
theocerdan <tcerdan.pro@gmail.com>
2025-03-27 17:01:51 +0100

    block log depth

--- src/address.tsx
+++ src/address.tsx
@@ -4,4 +4,4 @@ export const ROUTER_ADDRESS: Address = import.meta.env.VITE_ROUTER_ADDRESS;
 export const USDT_ADDRESS: Address = import.meta.env.VITE_USDT_ADDRESS;
 export const WETH_ADDRESS: Address = import.meta.env.VITE_WETH_ADDRESS;
 export const ZERO_ADDRESS: Address = '0x0000000000000000000000000000000000000000';
-export const LOG_DEPTH = 10000n;
+export const LOG_DEPTH = 100_000_0n;
